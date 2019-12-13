c26d39b041c501370d55beddbd60b2d4191c9447
thembones79 <michal.szumnarski@gmail.com>
2019-12-14 00:24:51 +0100

    done

--- src/index.js
+++ src/index.js
@@ -2,39 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 import Spinner from "./Spinner";
+import useLocation from "./useLocation"
 
-class App extends React.Component {
-  state = {
-    lat: null,
-    errorMessage: ""
-  };
+const App = () => {
 
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
-    );
+const [lat, errorMessage] = useLocation();
+  let content;
+  if (errorMessage) {
+    content = <div> Error: {errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else {
+    content = <Spinner message="Please accept location request 🥳" />;
   }
 
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    } else if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />;
-    } else if (!this.state.errorMessage && !this.state.lat) {
-      return <Spinner message="Please accept location request 🥳" />;
-    } else {
-      return <div>You're not supposed to see this!</div>;
-    }
-  }
-
-  render() {
-    return <div className="border red">{this.renderContent()}</div>;
-  }
-}
+  return <div className="border red">{content}</div>;
+};
 
 ReactDOM.render(<App />, document.querySelector("#root"));
